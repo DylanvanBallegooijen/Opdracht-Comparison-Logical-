ab493123af0e0126a86f02eca12b035b0181cb91
DylanvanBallegooijen <78920682+DylanvanBallegooijen@users.noreply.github.com>
2021-07-13 13:23:23 +0200

    Add files via upload

---
+++ Opdracht Comparison && Logical Operators/oplossing.js
@@ -0,0 +1,35 @@
+// Verander deze input om te zien of je code werkt.
+// Comment bijvoorbeeld regel 8 tot 11 weer aan, en regel 3 tot 5 uit.
+
+var age = >18 25;
+var firstname = bram;
+
+//const isFemale = false;
+//const driverStatus = "bob";
+
+/*
+Testdata
+const age = 16; 
+const isFemale = false;
+const driverStatus = 808; 
+*/
+
+if (age > 18  25) {
+    document.write("Je krijgt 50% korting op je biertje!!");
+} else {
+    document.write("Geen 50% korting!!");
+}
+
+if (firstname || bram || sarah) {
+    document.write("Je ticket zegt dat je een vrouw bent!");
+} else {
+    document.write(
+    "Helaas je hebt aangegeven op je ticket dat je een man bent, je mag niet naar binnen"
+  );
+}
+
+/*if (driverStatus === "bob") {
+    console.log("Je bent de bob, je mag autorijden");
+} else {
+    console.log("Je bent geen bob, hier met die autosleutels");
+}*/
